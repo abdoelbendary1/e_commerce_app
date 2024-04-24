b6cb78f03a9c9c0337ba224aaa9516c8854b6a80
Abdelrahman Elbendary <abdoelbendary8@gmail.com>
2024-04-25 00:05:38 +0200

    initial deveopment branch

--- lib/domain/repository/repository/auth_repository.dart
+++ lib/domain/repository/repository/auth_repository.dart
@@ -1,5 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/domain/entities/failures/failures.dart';
+import 'package:e_commerce_app/domain/entities/sign_up_repository_entitiy/sign_up_repository_entitiy.dart';
+
 abstract class AuthRepository {
-  void signUp(
+  Future<Either<Failures, SignUpRepositoryEntitiy>> signUp(
     String name,
     String email,
     String password,
